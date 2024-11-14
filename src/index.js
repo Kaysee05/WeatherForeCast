6211737dd5ec7d53b4dd79b9f43757a12081d7ef
Kaysee05 <159415699+Kaysee05@users.noreply.github.com>
2024-11-14 13:03:44 +0200

    Made a new call

--- src/index.js
+++ src/index.js
@@ -14,8 +14,9 @@ humidityElement.innerHTML = '${response.data.temperature.humidity}%';
 windSpeedElement.innerHTML = '$response.data.wind.speed}km/h';
 timeElement.innerHTML = formatDate(date);
    temperatureElement.innerHTML = Math.round(temperature);
-   
+ getForecast(response.data.city); 
 }
+
 
 function formatDate(date) {
 let minutes = date.getMinutes();
@@ -52,7 +53,14 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function getForecast(city) {
+let apiKey = "932e5ebeaff542505e2e03t63ba9boe8";
+let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+   console.log(response.data);
    
 
    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -82,5 +90,6 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-displayForecast();
+getForecast("Paris");
+
 
